116b1f659059905eb7497a98cebe8862683d81be
HJ03 <102145684+HJ03@users.noreply.github.com>
2025-08-20 00:05:28 +0530

    Connected local database

--- database.py
+++ database.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Replace with your PostgreSQL credentials
-# DATABASE_URL = "postgresql://postgres:billi007@localhost:5432/ecommerce_db"
+DATABASE_URL = "postgresql://postgres:billi007@localhost:5432/ecommerce_website_1"
 # DATABASE_URL = "postgresql://postgres:vnfCfvqRusHQdSFtYunNZjtEpyKobuzy@hopper.proxy.rlwy.net:30756/railway"
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:vnfCfvqRusHQdSFtYunNZjtEpyKobuzy@hopper.proxy.rlwy.net:30756/railway")
+
+## Railway Database
+# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:vnfCfvqRusHQdSFtYunNZjtEpyKobuzy@hopper.proxy.rlwy.net:30756/railway")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
